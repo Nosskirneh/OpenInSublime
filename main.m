e61c0fed8fc93173a97cf60f34ca6deb126d2e75
Andreas Henriksson <andreaskhenriksson@gmail.com>
2020-02-23 12:47:17 +0100

    Changed to Sublime Text

--- main.m
+++ main.m
@@ -7,48 +7,53 @@
 //
 
 #import <Cocoa/Cocoa.h>
-#include <objc/message.h>
 
-NSString *getPathToFrontFinderWindow()
-{
+@interface SBObject : NSObject
+- (SBObject *)target;
+- (SBObject *)get;
+- (NSString *)URL;
+@end
+
+@interface SBApplication : SBObject
++ (instancetype)applicationWithBundleIdentifier:(NSString *)bundleIdentifier;
+@end
+
+@interface _AMFinderApplication : SBApplication
+- (id)FinderWindows;
+@end
+
+NSString *getPathToFrontFinderWindow() {
     // Get Finder SBApplication instance
-    id finderApp = ((id (*)(id, SEL, NSString *))objc_msgSend)(objc_getClass("SBApplication"), sel_registerName("applicationWithBundleIdentifier:"), @"com.apple.Finder");
-    
+    _AMFinderApplication *finderApp = (_AMFinderApplication *)[SBApplication applicationWithBundleIdentifier:@"com.apple.Finder"];
+
     // Get array of all active Finder windows
-    NSArray *finderWindows = ((id (*)(id, SEL))objc_msgSend)(finderApp, sel_registerName("FinderWindows"));
-    
+    NSArray *finderWindows = finderApp.FinderWindows;
+
     // Get the target Finder window
     // TODO: handle multi montitor support somehow
-    id desiredFinderWindow = [finderWindows lastObject];
-    id desiredFinderWindowTarget = ((id (*)(id, SEL))objc_msgSend)(desiredFinderWindow, sel_registerName("target"));
-    desiredFinderWindowTarget = ((id (*)(id, SEL))objc_msgSend)(desiredFinderWindowTarget, sel_registerName("get"));
-    
+    SBObject *desiredFinderWindow = [finderWindows lastObject];
+
     // Get the path of the target finder window's working directory
-    NSString *finderCWD = ((id (*)(id, SEL))objc_msgSend)(desiredFinderWindowTarget, sel_registerName("URL"));
+    NSString *finderCWD = desiredFinderWindow.target.get.URL;
     finderCWD = [[NSURL URLWithString:finderCWD] path];
-    
+
     // Handle non-directory being focused
     BOOL isDir = NO;
     [[NSFileManager defaultManager] fileExistsAtPath:finderCWD isDirectory:&isDir];
     if (!isDir)
-    {
         finderCWD = [finderCWD stringByDeletingLastPathComponent];
-    }
-    
+
     return finderCWD;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     id pool = [[NSAutoreleasePool alloc] init];
-    
+
     NSString *path = getPathToFrontFinderWindow();
     if (path != nil)
-    {
-        [[NSTask launchedTaskWithLaunchPath:@"/usr/bin/open" arguments:@[@"-n", @"-b" ,@"com.microsoft.VSCode", @"--args", path]] waitUntilExit];
-    }
-    
+        [[NSTask launchedTaskWithLaunchPath:@"/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" arguments:@[path]] waitUntilExit];
+
     [pool release];
-    
+
     return 0;
 }
